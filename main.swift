f08c91e44beb353ffbb8c523bf4e2930cab70848
Alex Mitchell <alex@alexthemitchell.com>
2016-04-12 14:06:46 -0500

    Detail input file specs

--- main.swift
+++ main.swift
@@ -1,11 +1,37 @@
-// Use filename as Action identifier
-var totalDetectedIntents: UInt = 0
-var detectedIntents = [String : UInt]()
+typealias AAAction = String
 
-var Dictionary = AATrie()
+func main() {
+  // Use filename as Action identifier
+  var totalDetectedIntents: UInt = 0
+  var detectedIntents = [String : UInt]()
 
-// read input
+  var Dictionary = AATrie()
 
-func intentForWord(input: String) {
+  // read input
+
+}
+
+/**
+ * We assume the Word Intention File has been prepared by taking "allowable
+ *   words," mapping intents to each of these words, then adding phonetic and
+ *   typo-close words. The file should be formatted with the words in one column
+ *   followed by a tab, then a column of comma separated matching intents in
+ *   string format. This will be the key for the output hash table's
+ *   probability.
+ *
+ * e.g. ~/WordIntentions.map
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  quadratic   quadForm
+ *  kwadratic   quadForm
+ *  pythagorean pythTheorem
+ *  pytgahorean pythTheorem
+ *  theorem     pythTheorem
+ *  formula     quadForm,pythTheorem
+ */
+func loadWordIntentions(filepath: String) {
+
+}
+
+func intentForWord(word: String) -> AAAction {
 
 }
