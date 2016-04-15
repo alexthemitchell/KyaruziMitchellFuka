96978187fe24bbf922ff9800ab008bf128efe66c
Alex Mitchell <alex@alexthemitchell.com>
2016-04-15 10:47:11 -0500

    Fix input parsing

--- main.swift
+++ main.swift
@@ -2,7 +2,7 @@ import Foundation
 
 var intentions: AATrie!
 
-func probabilitiesForSentence(sentence: String) -> [AAAction : Double] {
+func probabilitiesForSentence(sentence: String) -> ([AAAction : Double], UInt) {
   var totalDetectedIntents: UInt = 0
   var detectedIntents = [AAAction : UInt]()
   for word in sentence.componentsSeparatedByString(" ") {
@@ -21,15 +21,15 @@ func probabilitiesForSentence(sentence: String) -> [AAAction : Double] {
     let percentage = Double(count)/Double(totalDetectedIntents)
     probabilities[intent] = percentage
   }
-  return probabilities
+  return (probabilities, totalDetectedIntents)
 }
 
 /**
  * We assume the Word Intention File has been prepared by taking "allowable
  *   words," mapping intents to each of these words, then adding phonetic and
  *   typo-close words. The file should be formatted with the words in one column
- *   followed by a tab, then a column of comma separated matching intents in
- *   string format. This will be the key for the output hash table's
+ *   followed by a four tabs, then a column of comma separated matching intents
+ *   in string format. This will be the key for the output hash table's
  *   probability.
  *
  * e.g. /home/user/WordIntentions.map
@@ -59,10 +59,10 @@ func parseFileText(fullText: String) -> AATrie {
   // The newline at the end of the file generates an empty pair
   pairs.removeLast()
   for pair in pairs {
-    let entries = pair.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-    print(entries)
+    let entries = pair.componentsSeparatedByString(" ")
     let keyword = entries[0]
     let actions = entries[1].componentsSeparatedByString(",")
+    print("\(keyword): \(actions)")
     trie.insertWord(keyword, actions: actions)
   }
   return trie
@@ -80,11 +80,19 @@ let arguments = Process.arguments
 let intentionPath = arguments[1]
 
 intentions = loadWordIntentions(intentionPath)
-
+print("Keywords loaded. Listening on STDIN")
 while true {
   if let sentence = readLine() {
-    let probabilities = probabilitiesForSentence(sentence)
-    let data = try! NSJSONSerialization.dataWithJSONObject(probabilities, options: .PrettyPrinted)
+    let start = NSDate()
+    let (probabilities, actionCount) = probabilitiesForSentence(sentence)
+    // processTime will be milliseconds it took to look up in the trie
+    let processTime = start.timeIntervalSinceNow * -1000
+    let output = [
+      "probabilities" : probabilities,
+      "actionWords" : actionCount,
+      "computeTime" : processTime
+    ]
+    let data = try! NSJSONSerialization.dataWithJSONObject(output, options: .PrettyPrinted)
     print(String(data: data, encoding: NSUTF8StringEncoding)!)
   }
 }
