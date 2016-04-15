ef91e0673ace3d7c0c93287894aedd79604b7767
Ajuna Kyaruzi <kyaruzia17@grinnell.edu>
2016-04-15 00:49:47 -0500

    Made typos work

--- src/libgenerator/Main.java
+++ src/libgenerator/Main.java
@@ -3,14 +3,16 @@
  */
 package libgenerator;
 
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.codec.language.DoubleMetaphone;
-
+import java.util.Queue;
 
 /**
  * @author Ajuna Kyaruzi
- * @lastModified April 10th, 2016
+ * @lastModified April 15th, 2016
  */
 public class Main {
 
@@ -23,7 +25,16 @@ public class Main {
 		String[] keywords = {"one", "two", "three"};
 		//figure out how to read into keywords
 	
-		PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter("the-file-name.txt", "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		for (String word : keywords) {
 			List<String> typoList = generateTypos(word);
@@ -56,54 +67,55 @@ public class Main {
 	}
 
 	private static List<String> generateTypos(String str) {
+			List<String> res = new LinkedList<String>();
+			String[] array_prox = new String[36];
+			int strLength = str.length();
+			
+		    array_prox['a'] = "qwzx";
+		    array_prox['b'] = "vfghn";
+		    array_prox['c'] = "xsdfv";
+		    array_prox['d'] = "xswerfvc";
+		    array_prox['e'] = "wsdfr";
+		    array_prox['f'] = "cdertgbv";
+		    array_prox['g'] = "rfvtbyhn";
+		    array_prox['h'] = "bgtyujmn";
+		    array_prox['i'] = "ujklo";
+		    array_prox['j'] = "nhyuikm";
+		    array_prox['k'] = "ujmlo";
+		    array_prox['l'] = "poikm";
+		    array_prox['m'] = "nhjkl";
+		    array_prox['n'] = "bghjm";
+		    array_prox['o'] = "iklp";
+		    array_prox['p'] = "ol";
+		    array_prox['r'] = "edfgt";
+		    array_prox['s'] = "qwezxc";
+		    array_prox['t'] = "rfghy";
+		    array_prox['u'] = "yhjki";
+		    array_prox['v'] = "cdfgb";    
+		    array_prox['w'] = "qasde";
+		    array_prox['x'] = "zasdc";
+		    array_prox['y'] = "tghju";
+		    array_prox['z'] = "xsa";
+		    array_prox['1'] = "qw";
+		    array_prox['2'] = "qwe";
+		    array_prox['3'] = "wer";
+		    array_prox['4'] = "ert";
+		    array_prox['5'] = "rty";
+		    array_prox['6'] = "tyu";
+		    array_prox['7'] = "yui";
+		    array_prox['8'] = "uio";
+		    array_prox['9'] = "iop";
+		    array_prox['0'] = "op";
 
-		    String array_prox[] = {};
-		    array_prox['a'] = {"q", "w", "z", "x"};
-		    array_prox['b'] = {'v', 'f', 'g', 'h', 'n'};
-		    array_prox['c'] = {'x', 's', 'd', 'f', 'v'};
-		    array_prox['d'] = {'x', 's', 'w', 'e', 'r', 'f', 'v', 'c'};
-		    array_prox['e'] = {'w', 's', 'd', 'f', 'r'};
-		    array_prox['f'] = {'c', 'd', 'e', 'r', 't', 'g', 'b', 'v'};
-		    array_prox['g'] = {'r', 'f', 'v', 't', 'b', 'y', 'h', 'n'};
-		    array_prox['h'] = {'b', 'g', 't', 'y', 'u', 'j', 'm', 'n'};
-		    array_prox['i'] = {'u', 'j', 'k', 'l', 'o'};
-		    array_prox['j'] = {'n', 'h', 'y', 'u', 'i', 'k', 'm'};
-		    array_prox['k'] = {'u', 'j', 'm', 'l', 'o'};
-		    array_prox['l'] = {'p', 'o', 'i', 'k', 'm'};
-		    array_prox['m'] = {'n', 'h', 'j', 'k', 'l'};
-		    array_prox['n'] = {'b', 'g', 'h', 'j', 'm'};
-		    array_prox['o'] = {'i', 'k', 'l', 'p'};
-		    array_prox['p'] = {'o', 'l'};
-		    array_prox['r'] = {'e', 'd', 'f', 'g', 't'};
-		    array_prox['s'] = {'q', 'w', 'e', 'z', 'x', 'c'};
-		    array_prox['t'] = {'r', 'f', 'g', 'h', 'y'};
-		    array_prox['u'] = {'y', 'h', 'j', 'k', 'i'};
-		    array_prox['v'] = {'', 'c', 'd', 'f', 'g', 'b'};    
-		    array_prox['w'] = {'q', 'a', 's', 'd', 'e'};
-		    array_prox['x'] = {'z', 'a', 's', 'd', 'c'};
-		    array_prox['y'] = {'t', 'g', 'h', 'j', 'u'};
-		    array_prox['z'] = {'x', 's', 'a'};
-		    array_prox['1'] = {'q', 'w'};
-		    array_prox['2'] = {'q', 'w', 'e'};
-		    array_prox['3'] = {'w', 'e', 'r'};
-		    array_prox['4'] = {'e', 'r', 't'};
-		    array_prox['5'] = {'r', 't', 'y'};
-		    array_prox['6'] = {'t', 'y', 'u'};
-		    array_prox['7'] = {'y', 'u', 'i'};
-		    array_prox['8'] = {'u', 'i', 'o'};
-		    array_prox['9'] = {'i', 'o', 'p'};
-		    array_prox['0'] = {'o', 'p'};
-
-		    String arr[] = {};
-
-			for(var a=0; a<str.length; a++) {
-			    var temp = array_prox[str.charAt(a)];    
-				for(var b=0; b<temp.length; b++) {
-				    var typo = str.replaceAt(a, temp[b]);
-				    arr.push(typo);
+			for (int a = 0; a < strLength; a++) {
+			    String temp = array_prox[str.charAt(a)];    
+			    int len = temp.length();
+				for (int b = 0; b < len ; b++) {
+				    String typo = replaceAt(str, a, temp.charAt(b));
+				    res.add(typo);
 				}
 			}
 
-			return arr;
+			return res;
 	}
 }
